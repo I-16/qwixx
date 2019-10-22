a5c8e86c339582443ae91cbebd006885aa47632a
Travis Molter <travismolter@gmail.com>
2019-10-22 09:45:35 -0700

    Pair programmed with Ben. Finished colored dice and roll dice function

--- play-page/dice-roll.js
+++ play-page/dice-roll.js
@@ -6,16 +6,15 @@ const rollDice = function() {
         output = '',
         diceCount = 6;
         // Index 0 and 1 are white dice, 2 is red, 3 is yellow, 4 is green and 5 is blue
-    let resultsArray = [];
+    let diceRollArray = [];
     for (i = 0; i < diceCount; i++) {
         faceValue = Math.floor(Math.random() * 6);
-        output += '&#x268' + faceValue + '; ';
-        resultsArray.push(faceValue + 1);
+        output += '<span class="die">' + '&#x268' + faceValue + ';</span>';
+        diceRollArray.push(faceValue + 1);
     }
-    document.getElementById('common-die-1').innerHTML = output;
+    document.getElementById('dice-display').innerHTML = output;
     faceValue++;
-    return faceValue;
-    
+    return diceRollArray; 
 };
 
-export default rollDice;
+// export default rollDice;
