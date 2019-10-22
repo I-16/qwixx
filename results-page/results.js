70c1137e34ed69f9b07225cc666da53a75182c0c
sethripman <sethripman@gmail.com>
2019-10-22 14:55:57 -0700

    finished basic results logic and tested in browser pair seth + travis

--- results-page/results.js
+++ results-page/results.js
@@ -10,20 +10,21 @@ let sessionData = getUser();
 let userName = sessionData.name;
 let userScore = sessionData.sessionScore;
 let newHighScoreFlag = false;
-
+debugger;
 //if high score is empty, create a new high score
 if (getHighScore() === null) {
     let defaultHighScore = makeHighScore(userName, userScore); // make a high score with the key score and the value of an object with the key-values in the parameters
     saveHighScore(defaultHighScore);
     // new high score css flag for Ben's animations
     newHighScoreFlag = true;
+    debugger;
 }
 
 // retrieve high score
 let highScore = getHighScore();
 let highScoreUserName = highScore.name;
 let highScoreUserScore = highScore.sessionScore;
-
+debugger;
 // compare the two scores, make new high score if this is a new high score
 if (highScoreUserScore < userScore) {
     // save user score as the new high score
@@ -32,6 +33,7 @@ if (highScoreUserScore < userScore) {
     
     // new high score css flag for ben's animations
     newHighScoreFlag = true;
+    debugger;
 }
 
 // display user score and high score
@@ -39,3 +41,4 @@ userNameSpan.textContent = userName;
 userScoreSpan.textContent = userScore;
 highScoreNameSpan.textContent = highScoreUserName;
 highScoreScoreSpan.textContent = highScoreUserScore;
+debugger;
