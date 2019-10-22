6a550c4d8ea2b22fa8a1456ef13a21eb9e458604
Travis Molter <travismolter@gmail.com>
2019-10-22 14:08:31 -0700

    working on local storage, was working, now its not.

--- landing-page/home.js
+++ landing-page/home.js
@@ -1,29 +1,17 @@
+
+const userSignUp = document.getElementById('user-sign-up');
+
 function saveUser(user) {
     const json = JSON.stringify(user);
     localStorage.setItem('user', json);
 }
-
-function getUser() {
-    const json = localStorage.getItem('user');
-    if (!json) return null;
-    const user = JSON.parse(json);
-    return user;
-}
-
 function makeUser(formData) {
     const user = {
-        name: formData.get('user-name'),
+        name: formData.get('name'),
         sessionScore: 0,
     };
     return user;
 }
-
-const makeNewUser = (formdata) => ({
-    name: formdata.get('user-name'),
-    sessionScore: 0
-});
-
-const userSignUp = document.getElementById('user-sign-up');
 
 userSignUp.addEventListener('submit', function(event) {
     event.preventDefault();
