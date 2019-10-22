fa8950ce4e5d8f1d094602c6bc76dad86cf3ed18
drmeloy <drmeloy@gmail.com>
2019-10-22 15:31:00 -0600

    working on submit button

--- play-page/play.js
+++ play-page/play.js
@@ -1,13 +1,52 @@
-import { generateAllRows } from './gen-all-rows.js';
+import { generateAllRows } from './gen-all-rows.js';    
+import { disableLeftInputs } from './dis-left-inputs.js';
+import { countChecks, scoreColor, scorePenalties } from '../common/utils.js';
 
 const redRow = document.getElementById('red-row');
 const yellowRow = document.getElementById('yellow-row');
 const greenRow = document.getElementById('green-row');
 const blueRow = document.getElementById('blue-row');
+const redScoreDisplay = document.getElementById('red-score');
+const yellowScoreDisplay = document.getElementById('yellow-score');
+const greenScoreDisplay = document.getElementById('green-score');
+const blueScoreDisplay = document.getElementById('blue-score');
+const penaltyScoreDisplay = document.getElementById('penalty-score');
+const confirmButton = document.getElementById('confirm-button');
 
-[redRow, yellowRow, greenRow, blueRow].forEach(row => {
+const allRows = [redRow, yellowRow, greenRow, blueRow];
+
+allRows.forEach(row => {
     const newRow = generateAllRows(row.id);
     newRow.forEach(box => {
         row.appendChild(box);
     });
 });
+
+const updateScoresDisplay = (scoreArray) => {
+    redScoreDisplay.textContent = scoreArray[0];
+    yellowScoreDisplay.textContent = scoreArray[1];
+    greenScoreDisplay.textContent = scoreArray[2];
+    blueScoreDisplay.textContent = scoreArray[3];
+    penaltyScoreDisplay.textContent = scoreArray[4];
+};
+
+const confirmClick = () => {
+    let scoreArray = [];
+    
+    allRows.forEach(row => {
+        disableLeftInputs(row);
+        const numberOfChecks = countChecks(row);
+        const colorScore = scoreColor(numberOfChecks);
+        scoreArray.push(colorScore);
+    });
+    console.log(scoreArray);
+
+    const penaltyBoxArray = document.querySelectorAll('input[type=checkbox].penalty');
+    const penaltyCount = countChecks(penaltyBoxArray);
+    const penaltyScore = scorePenalties(penaltyCount);
+    scoreArray.push(penaltyScore);
+
+    updateScoresDisplay(scoreArray);
+};
+
+confirmButton.addEventListener('click', confirmClick);
