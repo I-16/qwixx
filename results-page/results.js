6f24292007ba6638c7aa8cce1947852e095f6fed
sethripman <sethripman@gmail.com>
2019-10-22 14:05:32 -0700

    began writing results page logic

---
+++ results-page/results.js
@@ -0,0 +1,8 @@
+// declare previous stored high score and current session score
+let highScore = getHighScore(); // user object - name, sessionScore obj properties
+let sessionData = getUser(); // high score object - name, sessionScore obj properties
+
+// compare the two scores, make new high score if this is a new high score
+if (highScore.sessionScore < sessionData.sessionScore) {
+
+}
