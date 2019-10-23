4d7d5a2f9bca066e67b10443f74f11ac6548253a
sethripman <sethripman@gmail.com>
2019-10-22 17:12:26 -0700

    wrote endgame function

--- play-page/end-game.js
+++ play-page/end-game.js
@@ -1,3 +1,12 @@
-export const endGame = () => {
-    
-}
+import { saveUser } from '../common/utils.js';
+
+export const endGame = (userName, userSessionScore) => {
+    const user = {
+        name: userName,
+        sessionScore: userSessionScore,
+    };
+
+    saveUser(user);
+
+    window.location.assign('../results-page/index.html');
+};
