e35f83674dcf5a3ba13e3e77fa579247e1580e42
Travis Molter <travismolter@gmail.com>
2019-10-25 12:01:32 -0700

    results page working again

--- results-page/results.js
+++ results-page/results.js
@@ -19,17 +19,22 @@ if (getHighScore() === null) {
     newHighScoreFlag = true;
 }
 
+// display user score and high score
+userNameSpan.textContent += userName;
 // retrieve high score
 let highScore = getHighScore();
 let highScoreUserName = highScore.name;
 let highScoreUserScore = highScore.sessionScore;
+userScoreSpan.textContent += userScore;
+highScoreNameSpan.textContent += highScoreUserName;
+highScoreScoreSpan.textContent += highScoreUserScore;
 
 // compare the two scores, make new high score if this is a new high score
 if (highScoreUserScore < userScore) {
     // save user score as the new high score
     let newHighScore = makeHighScore(userName, userScore);
     saveHighScore(newHighScore);
-
+    
     // new high score css flag for ben's animations
     newHighScoreFlag = true;
 }
@@ -38,15 +43,15 @@ if (newHighScoreFlag === true) {
     userNameSpan.classList.add('animated');
     userNameSpan.classList.add('bounceInDown');
     userNameSpan.classList.add('delay-1s');
-
+    
     userScoreSpan.classList.add('animated');
     userScoreSpan.classList.add('bounceInDown');
     userScoreSpan.classList.add('delay-2s');
-
+    
     highScoreNameSpan.classList.add('animated');
     highScoreNameSpan.classList.add('bounceInDown');
     highScoreNameSpan.classList.add('delay-3s');
-
+    
     highScoreScoreSpan.classList.add('animated');
     highScoreScoreSpan.classList.add('bounceInDown');
     highScoreScoreSpan.classList.add('delay-3s');
@@ -60,8 +65,3 @@ function playAudio() {
     playHighScore.play();
 }
 
-// display user score and high score
-userNameSpan.textContent += userName;
-userScoreSpan.textContent += userScore;
-highScoreNameSpan.textContent += highScoreUserName;
-highScoreScoreSpan.textContent += highScoreUserScore;
