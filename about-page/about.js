e011bec43acd238c17ebb05a908942246370caee
sethripman <sethripman@gmail.com>
2019-10-24 18:55:47 -0700

    fresh dev!

--- about-page/about.js
+++ about-page/about.js
@@ -2,6 +2,14 @@ const sethFaces = document.querySelectorAll('.seth-face');
 const danFaces = document.querySelectorAll('.dan-face');
 const benFaces = document.querySelectorAll('.ben-face');
 const travisFaces = document.querySelectorAll('.travis-face');
+const sethHidden = document.getElementById('hidden-seth');
+const danHidden = document.getElementById('hidden-dan');
+const benHidden = document.getElementById('hidden-ben');
+const travisHidden = document.getElementById('hidden-travis');
+const sethHiddenImg = document.getElementById('big-seth');
+const danHiddenImg = document.getElementById('big-dan');
+const benHiddenImg = document.getElementById('big-ben');
+const travisHiddenImg = document.getElementById('big-travis');
 
 const sethReveal = () => {
     alert('seth');
@@ -35,20 +43,28 @@ const travisConceal = () => {
 
 sethFaces.forEach(function(item){
     item.addEventListener('mouseover', sethReveal);
-    item.addEventListener('mouseout', sethConceal);
 });
 
 danFaces.forEach(function(item){
     item.addEventListener('mouseover', danReveal);
-    item.addEventListener('mouseout', danConceal);
 });
 
 benFaces.forEach(function(item){
     item.addEventListener('mouseover', benReveal);
-    item.addEventListener('mouseout', benConceal);
 });
 
 travisFaces.forEach(function(item){
     item.addEventListener('mouseover', travisReveal);
-    item.addEventListener('mouseout', travisConceal);
 });
+
+sethHiddenImg.addEventListener('mouseout', sethConceal);
+danHiddenImg.addEventListener('mouseout', danConceal);
+benHiddenImg.addEventListener('mouseout', benConceal);
+travisHiddenImg.addEventListener('mouseout', travisConceal);
+
+/*
+on conceal - parent.setAttribute(visibility none)
+remove attribute from 
+
+
+*/
